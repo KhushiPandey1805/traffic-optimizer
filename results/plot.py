7910a0d2746038a7ad3a35f8aad8d2e3d48b082a
Khushi Pandey <khushipan1805@gmail.com>
2026-06-30 20:08:55 +0000

    Added results for dynamic control, not as great as adaptive currently

--- results/plot.py
+++ results/plot.py
@@ -3,15 +3,19 @@ import matplotlib.pyplot as plt
 
 fixed = pd.read_csv("fixed.csv")
 adaptive = pd.read_csv("adaptive.csv")
+dynamic = pd.read_csv("dynamic.csv")
 
 fixed["smooth"] = fixed["wait"].rolling(10).mean()
 adaptive["smooth"] = adaptive["wait"].rolling(10).mean()
+dynamic["smooth"]=dynamic["wait"].rolling(10).mean()
 
 print("Fixed avg wait:", fixed["wait"].mean())
 print("Adaptive avg wait:", adaptive["wait"].mean())
+print("Dynamic avg wait:", dynamic["wait"].mean())
 
 print("Fixed max wait:", fixed["wait"].max())
 print("Adaptive max wait:", adaptive["wait"].max())
+print("Dynamic max wait:", dynamic["wait"].max())
 
 plt.plot(
     fixed["time"],
@@ -23,6 +27,12 @@ plt.plot(
     adaptive["time"],
     adaptive["smooth"],
     label="Adaptive"
+)
+
+plt.plot(
+    dynamic["time"],
+    dynamic["smooth"],
+    label="Dynamic"
 )
 
 plt.xlabel("Time")
