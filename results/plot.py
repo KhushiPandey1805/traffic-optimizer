b3b70c15eb5140e47ba27daa8ebaf1d4d87293ea
Khushi Pandey <khushipan1805@gmail.com>
2026-07-01 20:16:13 +0000

    Added qlearning results and comparison

--- results/plot.py
+++ results/plot.py
@@ -4,18 +4,22 @@ import matplotlib.pyplot as plt
 fixed = pd.read_csv("fixed.csv")
 adaptive = pd.read_csv("adaptive.csv")
 dynamic = pd.read_csv("dynamic.csv")
+qlearning=pd.read_csv("qlearning.csv")
 
 fixed["smooth"] = fixed["wait"].rolling(10).mean()
 adaptive["smooth"] = adaptive["wait"].rolling(10).mean()
 dynamic["smooth"]=dynamic["wait"].rolling(10).mean()
+qlearning["smooth"]=qlearning["wait"].rolling(10).mean()
 
 print("Fixed avg wait:", fixed["wait"].mean())
 print("Adaptive avg wait:", adaptive["wait"].mean())
 print("Dynamic avg wait:", dynamic["wait"].mean())
+print("Q-learning avg wait time:",qlearning["wait"].mean())
 
 print("Fixed max wait:", fixed["wait"].max())
 print("Adaptive max wait:", adaptive["wait"].max())
 print("Dynamic max wait:", dynamic["wait"].max())
+print("Q-learning max wait:",qlearning["wait"].max())
 
 plt.plot(
     fixed["time"],
@@ -33,6 +37,12 @@ plt.plot(
     dynamic["time"],
     dynamic["smooth"],
     label="Dynamic"
+)
+
+plt.plot(
+    qlearning["time"],
+    qlearning["smooth"],
+    label="Q-learning"
 )
 
 plt.xlabel("Time")
